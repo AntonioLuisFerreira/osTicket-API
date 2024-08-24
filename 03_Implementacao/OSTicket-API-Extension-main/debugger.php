e7a60497ffbd0ba09ced79acf51dfc52333ff60d
AntonioLuisFerreira <a.m.v.l.ferreira@gmail.com>
2024-08-24 02:34:13 +0100

    new file:   03_Implementacao/OSTicket-API-Extension 	deleted:    03_Implementacao/OSTicket-API-Extension-main/.htaccess 	deleted:    03_Implementacao/OSTicket-API-Extension-main/api/api.extension.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/api/class.api.extension.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/class.ticket.extension.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/config.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/debug.txt 	deleted:    03_Implementacao/OSTicket-API-Extension-main/debugger.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/extension.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/plugin.config.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/plugin.php 	deleted:    03_Implementacao/OSTicket-API-Extension-main/sql/savedData.sql 	deleted:    03_Implementacao/OSTicket-API-Extension-main/sql/scripts.sql 	deleted:    03_Implementacao/OSTicket-API-Extension-main/sql/uninstallScript.sql 	deleted:    03_Implementacao/OSTicket-API-Extension-main/util/table.installer.php

--- 03_Implementacao/OSTicket-API-Extension-main/debugger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Debugger{
-    static function debugToFile($erro){
-        $file = PRJ_PLUGIN_DIR."debug.txt";
-        $text =  $erro."\n";
-        file_put_contents($file, $text, FILE_APPEND | LOCK_EX);
-    }
-}
